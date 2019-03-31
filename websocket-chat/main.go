50c44fcbfac2092821f5002522ec79444fb97d64
三上大河 <mikamimikami60@gmail.com>
2019-03-31 16:37:21 +0900

    main makes chat room

--- websocket-chat/main.go
+++ websocket-chat/main.go
@@ -20,8 +20,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
 
+
+	// get the room going
+	go r.run()
+
+	// start the web server
 	log.Println("Server is running")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
