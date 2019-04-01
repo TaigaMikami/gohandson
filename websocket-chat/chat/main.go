bafd12d6dbc7fd0174d723d4bd97b5805ac1c062
三上大河 <mikamimikami60@gmail.com>
2019-04-01 13:37:09 +0900

    add login page

--- websocket-chat/chat/main.go
+++ websocket-chat/chat/main.go
@@ -27,14 +27,19 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	var is_trace = flag.String("is_trace", "true", "トレース")
 	flag.Parse()
+	gomniauth.SetSecurityKey("98dfbg7iu2nb4uywevihjw4tuiyub34noilk")
+	gomniauth.WithProviders(
+		github.New("320540c03df1cda7aa65", "33e078f10d34087e93a82377f81a6731799a4bc0", "http://localhost:8080/auth/callback/github"),
+	)
 	r := newRoom()
 
 	if *is_trace == "true" {
 		r.tracer = trace.New(os.Stdout)
 	}
 	http.Handle("/", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
-
 
 	// get the room going
 	go r.run()
