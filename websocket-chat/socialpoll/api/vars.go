51ae8d6d5e5c7f706ca9dfc71835a34f2eac31c2
三上大河 <mikamimikami60@gmail.com>
2019-04-26 15:11:30 +0900

    tmp

---
+++ websocket-chat/socialpoll/api/vars.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"sync"
+)
+
+var (
+	varsLock sync.RWMutex
+	vars map[*http.Request]map[string]interface{}
+)
+func OpenVars(r *http.Request) {
+	varsLock.Lock()
+	if vars == nil {
+		vars = map[*http.Request]map[string]interface{}{}
+	}
+	vars[r] = map[string]interface{}{}
+	varsLock.Unlock()
+}
+
+func CloseVars(r *http.Request) {
+	varsLock.Lock()
+	delete(vars, r)
+	varsLock.Unlock()
+}
+
+func GetVar(r *http.Request, key string) interface{} {
+	varsLock.RLock()
+}
