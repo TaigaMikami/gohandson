3304e8accfeae87ddd1cb90952a440608d426b07
三上大河 <mikamimikami60@gmail.com>
2019-04-27 16:54:40 +0900

    update meander

--- websocket-chat/meander/cost_level_test.go
+++ websocket-chat/meander/cost_level_test.go
@@ -2,7 +2,8 @@ package meander_test
 
 import (
 	"testing"
-	""
+	"github.com/cheekybits/is"
+	"github.com/TaigaMikami/gohandson/websocket-chat/meander"
 )
 
 func TestCostValues(t *testing.T) {
@@ -12,4 +13,22 @@ func TestCostValues(t *testing.T) {
 	is.Equal(int(meander.Cost3), 3)
 	is.Equal(int(meander.Cost4), 4)
 	is.Equal(int(meander.Cost5), 5)
+}
+
+func TestCostString(t *testing.T) {
+	is := is.New(t)
+	is.Equal(meander.Cost1.String(), "$")
+	is.Equal(meander.Cost2.String(), "$$")
+	is.Equal(meander.Cost3.String(), "$$$")
+	is.Equal(meander.Cost4.String(), "$$$$")
+	is.Equal(meander.Cost5.String(), "$$$$$")
+}
+
+func TestParseCost(t *testing.T) {
+	is := is.New(t)
+	is.Equal(meander.Cost1, meander.ParseCost("$"))
+	is.Equal(meander.Cost2, meander.ParseCost("$$"))
+	is.Equal(meander.Cost3, meander.ParseCost("$$$"))
+	is.Equal(meander.Cost4, meander.ParseCost("$$$$"))
+	is.Equal(meander.Cost5, meander.ParseCost("$$$$$"))
 }
